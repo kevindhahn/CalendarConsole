44bc64e8857a6811fe7c5b3b69ab1b615279d519
kevindhahn <66223213+kevindhahn@users.noreply.github.com>
2020-06-01 12:04:02 -0500

    Add files via upload

---
+++ CalendarTests/UTCalendar.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalendarTests
+{
+    [TestClass]
+    public class UTCalendar
+    {
+/*
+        [TestMethod]
+        public void SixWeekMiddle()
+        {
+            DateTime value = new DateTime(2020,5,12);
+            int expected = 6;
+
+            Calendar.MyMonth cal = new Calendar.MyMonth(value);
+            int result = cal.NumberOfWeeks(DayOfWeek.Sunday);
+
+            Assert.AreEqual(result, expected, "5 month week Failed");
+        }
+
+        [TestMethod]
+        public void SixWeekSaturday()
+        {
+            DateTime value = new DateTime(2020, 5, 23);
+            int expected = 6;
+
+            Calendar.MyMonth cal = new Calendar.MyMonth(value);
+            int result = cal.NumberOfWeeks(DayOfWeek.Sunday);
+
+            Assert.AreEqual(result, expected, "5 month week Failed");
+        }
+
+        [TestMethod]
+        public void SixWeekSunday()
+        {
+            DateTime value = new DateTime(2020, 5, 24);
+            int expected = 6;
+
+            Calendar.MyMonth cal = new Calendar.MyMonth(value);
+            int result = cal.NumberOfWeeks(DayOfWeek.Sunday);
+
+            Assert.AreEqual(result, expected, "5 month week Failed");
+        }
+
+        [TestMethod]
+        public void SixWeekMonday()
+        {
+            DateTime value = new DateTime(2020, 5, 25);
+            int expected = 6;
+
+            Calendar.MyMonth cal = new Calendar.MyMonth(value);
+            int result = cal.NumberOfWeeks(DayOfWeek.Sunday);
+
+            Assert.AreEqual(result, expected, "5 month week Failed");
+        }
+*/
+    }
+}
